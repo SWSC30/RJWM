6af4ee2421cea5b7a40449d78ae91a219e3df4ba
SGG_git <SGG_git@.com>
2023-08-23 17:21:05 +0800

    使用spring cache缓存套餐数据

--- src/main/java/com/itzq/reggie/controller/SetmealController.java
+++ src/main/java/com/itzq/reggie/controller/SetmealController.java
@@ -16,6 +16,8 @@ import com.itzq.reggie.service.impl.CategoryServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,6 +44,7 @@ public class SetmealController {
     private CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value="setmealCache",allEntries = true)
     public R<String> save (@RequestBody SetmealDto setmealDto){
         log.info("套餐信息:{}",setmealDto);
 
@@ -145,6 +148,7 @@ public class SetmealController {
     * 删除套餐
     * */
     @DeleteMapping
+    @CacheEvict(value="setmealCache",allEntries = true)
     public R<String> delete(@RequestParam("id") List<Long> ids){
         log.info("ids:{}",ids);
         //删除setmeal表的信息
@@ -175,6 +179,7 @@ public class SetmealController {
         }
     }
     @GetMapping("/list")
+    @Cacheable(value="setmealCache",key="#setmeal.categoryId + '_'+ #setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
 
